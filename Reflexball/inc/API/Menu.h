c3bdd4d105f610ec60c325acb096470d810a1374
Frederik Skov <FrederikSkov@frederiksmbp19.eduroam.wireless.dtu.dk>
2018-06-11 13:33:22 +0200

    j

---
+++ Reflexball/inc/API/Menu.h
@@ -0,0 +1,8 @@
+
+#ifndef _MENU_H_
+#define _MENU_H_
+#include "ansi.h"
+
+
+void PrintTitle(char ASCIITitle,int titleX, int titleY);
+ #endif
--- Reflexball/src/API/Menu.c
+++ Reflexball/src/API/Menu.c
@@ -1,5 +1,16 @@
-void PrintTitle(){
+void PrintTitle(char ASCIITitle[], uint8_t titleX, uint8_t titleY) {
+  // Move cursor to (titleX,titleY)
+  gotoXY(titleX,titleY);
 
+  //size_t NumberOfElements = sizeof(ASCIITitle)/sizeof(ASCIITitle[0]);
+
+  // Run trough columns
+  for (int RowI = 0; RowI < 5; RowI++) {
+    for (int ColI = 0; ColI < 9; ColI++) {
+
+      
+    }
+  }
 
 }
 
