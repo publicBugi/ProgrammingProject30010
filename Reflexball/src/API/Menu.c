de10a6bfe3ee10e41818abf32811e4f434a466cd
Frederik Skov <FrederikSkov@frederiksmbp19.eduroam.wireless.dtu.dk>
2018-06-11 14:17:45 +0200

    new menu

--- Reflexball/src/API/Menu.c
+++ Reflexball/src/API/Menu.c
@@ -1,27 +1,55 @@
 
+
+// Print text fra 2D array.
+void PrintOutTextArray(char TextArray[][]) {
+//  int total = sizeof(TextArray);
+  //int col = sizeof(TextArray[0]);
+//  int row = total / col;
+
+  int MaxColI = sizeof(TextArray) / sizeof(ASCIITitle[0][0]);
+  int MaxRowI = sizeof(TextArray) / MaxColI;
+
+  // Run trough columns.
+  for (int RowI = 0; RowI < MaxRowI; RowI++) {
+
+    // Run trough rows.
+    for (int ColI = 0; ColI < MaxColI; ColI++) {
+
+      // Print out char from text array.
+      printf("%c", TextArray[RowI][ColI]);
+
+    }
+
+  }
+}
+
+
 // Print title.
 void PrintTitle(char ASCIITitle[][], uint8_t titleX, uint8_t titleY) {
-  
+
   // Move cursor to (titleX,titleY)
   gotoXY(titleX,titleY);
 
-  //size_t NumberOfElements = sizeof(ASCIITitle)/sizeof(ASCIITitle[0]);
-
-  // Run trough columns.
-  for (int RowI = 0; RowI < 5; RowI++) {
-
-    // Run trough rows.
-    for (int ColI = 0; ColI < 9; ColI++) {
-
-      // Print out char of title.
-      printf("%c", ASCIITitle[RowI][ColI]);
-
-    }
-  }
+  // Print
+  PrintOutTextArray(ASCIITitle);
 
 }
 
-void PrintMenu() {
+// Print the menu options.
+void PrintMenu(int state, char ASCIIArray[][][]) {
+  char From, To;
+// ASCIIArray[] = ["Play game", "Highscore", "Help", "1 Player", "2. Player", "Initial","Score"]
+  switch(state) {
+
+      case 1: // Play menu
+        From = 0;
+        To = 2;
+
+      case 2: // Highscore menu
+        From = 3;
+        To = 4;
+  }
+
 
 }
 
