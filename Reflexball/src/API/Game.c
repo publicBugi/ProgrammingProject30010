5cf51bbc59fdaf6a74428ce2622ddc8a19943cb1
SebRydahl <sebrydahl@gmail.com>
2018-06-12 16:09:50 +0200

    Fixed a merge problem with a continued branch

--- Reflexball/src/API/Game.c
+++ Reflexball/src/API/Game.c
@@ -32,11 +32,22 @@ void drawBall() {
 //    putchar(111);
 //}
 
-void CountDown(){
-
-
-
+void CountDown(char *numberArray[][][]){
+  // Start at 4 to make sure the first second is at least a full second
+  for (int i = 4; i > 0; i--) {
+    uint8_t clkSec = clk->time_sec;
+    while(clkSec == clk->time_sec){}
+    // For the first value of I, print 3
+    if (i==4) {
+      PrintOutTextArray(numberArray[3][][], *countDownX, *countDownY, 5, 8);
+    }
+    // Print the rest of the numbers, every time the time_sec changes
+    else{
+      PrintOutTextArray(numberArray[i][][], *countDownX, *countDownY, 5, 8);
+    }
+  }
 }
+
 int CollisionDetect() {
 
 
