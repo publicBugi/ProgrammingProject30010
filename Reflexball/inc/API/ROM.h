676a52917e5676a002a7351bc175dfbdd6776101
SebRydahl <sebrydahl@gmail.com>
2018-06-14 10:50:36 +0200

    Initiate and write defined
    Current version should now be able to clear the flash when it is initiating and write to the the flash when passing an Array[][][]

--- Reflexball/inc/API/ROM.h
+++ Reflexball/inc/API/ROM.h
@@ -1,1 +1,4 @@
 #include "stm32f30x_conf.h"
+
+#define ADDRESS 0x0800F800
+#define ASCIICHARARRAY data[36][4][10]
--- Reflexball/src/API/ROM.c
+++ Reflexball/src/API/ROM.c
@@ -1,8 +1,21 @@
 #include "ROM.h"
 
-void WriteToFlash() {
-FLASH_Unlock();
-FLASH_ClearFlag( FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPERR );
-FLASH_ErasePage(0x0800F800);
+void InitFlash() {
+    FLASH_Unlock(); //Unlock the flash
+    FLASH_ClearFlag( FLASH_FLAG_EOP | FLASH_FLAG_PGERR | FLASH_FLAG_WRPERR );
+    FLASH_ErasePage(ADDRESS); //Erase the flash storage on the last address
+    FLASH_Lock(); //Lock the flash
 }
 
+void WriteToFlash(uint8_t ASCIICHARARRAY) {
+    uint16_t location = 0;
+    FLASH_Unlock();
+    for (uint8_t i = 0; i < 36; i++) {
+        for (uint8_t j = 0; j < 4; j++) {
+            for (uint8_t k = 0; k < 10; k++) {
+                FLASH_ProgramHalfWord(ADDRESS+location*2, data[i][j][k]);
+            }
+        }
+    }
+    FLASH_Lock();
+}
